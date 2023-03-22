e632ffbe82245b0543bdf6bd36248895eb4a3173
Zain-OP <128605388+Zain-OP@users.noreply.github.com>
2023-03-22 18:39:29 +0530

    Create exampleconfig.py

---
+++ exampleconfig.py
@@ -0,0 +1,15 @@
+from sample_config import Config
+
+
+class Development(Config):
+  API_ID =  
+  API_HASH = 
+  BOT_TOKEN = 
+  MONGO_URL = 
+  DATABASE_NAME = 
+  BOT_USERNAME = 
+  OWNER_USERNAME = 
+  BOT_NAME = 
+  ADMINS = 
+  START_IMG = 
+  STKR = 
