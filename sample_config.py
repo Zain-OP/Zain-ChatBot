ea6c1318c5a874de090f878f32f6d2c9bf6d9235
Zain-OP <128605388+Zain-OP@users.noreply.github.com>
2023-03-22 18:41:19 +0530

    Create sample_config.py

---
+++ sample_config.py
@@ -0,0 +1,14 @@
+import os
+
+class Config(object):
+  API_ID = os.environ.get("API_ID", None)  
+  API_HASH = os.environ.get("API_HASH", None) 
+  BOT_TOKEN = os.environ.get("BOT_TOKEN", None) 
+  MONGO_URL = os.environ.get("MONGO_URL", None)
+  DATABASE_NAME = os.environ.get("DATABASE_NAME") 
+  BOT_USERNAME = os.environ.get("BOT_USERNAME") 
+  OWNER_USERNAME = os.environ.get("OWNER_USERNAME")
+  BOT_NAME = os.environ.get("BOT_NAME")
+  ADMINS = os.environ.get("ADMINS")
+  START_IMG = os.environ.get("START_IMG")
+  STKR = os.environ.get("STKR")
